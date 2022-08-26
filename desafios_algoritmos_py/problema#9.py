44657c9ad8aa4a0f9255813fed9a4fdc96d7e544
unknown <LucasFeliciano02@outlook.com>
2022-08-26 07:34:28 -0300

    Some os valores de uma colecao

---
+++ desafios_algoritmos_py/problema#9.py
@@ -0,0 +1,10 @@
+"""
+* PROBLEMA:  Some os valores de uma colecao
+"""
+
+idades = [15, 32, 12, 20, 50, 35]
+total = 0
+
+for idade in idades:
+    total = total + idade  # 0 + 15 = 15 | 15 + 32 = 47 | 47 + 12 ....
+print(total)
