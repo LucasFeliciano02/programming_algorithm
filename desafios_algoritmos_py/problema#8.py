1c281e426da53e10b7b071413bdf2fe34b5cd5e5
unknown <LucasFeliciano02@outlook.com>
2022-08-26 07:33:41 -0300

    Imprima valores de 1 a N

---
+++ desafios_algoritmos_py/problema#8.py
@@ -0,0 +1,9 @@
+"""
+* PROBLEMA:  Imprima valores de 1 a N
+"""
+
+valor_maximo = int(input('digite o valor máximo: '))
+valor_inicial = 1
+
+for numero in range(valor_inicial, valor_maximo + 1):
+    print(numero)
