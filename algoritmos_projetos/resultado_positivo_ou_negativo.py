4c505599c99015ec87c906ee77f482e48ec04438
unknown <LucasFeliciano02@outlook.com>
2022-08-29 08:33:15 -0300

    Converte um valor para positivo multiplicando por -1 caso for < que 0

---
+++ algoritmos_projetos/resultado_positivo_ou_negativo.py
@@ -0,0 +1,9 @@
+n = int(input('Digite um valor inteiro: '))
+
+if n >= 0:
+    print(f'{n} é um número positivo')
+else:
+    print(f'-{n * -1} foi convertido em {n*-1} que é um numero positivo')
+
+
+# Converte um valor para positivo multiplicando por -1 caso for < que 0
