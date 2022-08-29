322cd41e4e474025b3ae904d3adebe85290fd0cf
unknown <LucasFeliciano02@outlook.com>
2022-08-29 08:34:04 -0300

    Quantos salarios minimos a pessoa ganha

---
+++ algoritmos_projetos/quantos_salarios_minimos.py
@@ -0,0 +1,8 @@
+salario_pessoa = float(input('Digite seu salario: '))
+salario_minimo = 1212
+
+
+media_salarial_comparada = (salario_pessoa / salario_minimo)
+
+
+print(f'Você ganha: {media_salarial_comparada:.2f} salários minimos')
