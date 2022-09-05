5b8ca0dab7e711ae573111f7c07c9933e771b3bf
unknown <LucasFeliciano02@outlook.com>
2022-09-05 13:20:33 -0300

    Exibir somente as propriedades do tipo string que estão nesse objeto

---
+++ desafios_projetos_em_javascript/mini-projeto_6/encontre_o_string.js
@@ -0,0 +1,24 @@
+// Criar um método para ler propriedades de um objeto
+// Exibir somente as propriedades do tipo string que estão nesse objeto
+
+
+const filme = {
+    titulo : 'Vingadores',
+    ano : 2018,
+    diretor : 'Claudio',
+    personagem : 'Thor'
+}
+
+exibirPropriedades(filme);
+function exibirPropriedades(obj) {
+    for (prop in obj)
+        if(typeof obj[prop] === 'string')
+            console.log(prop, obj[prop])
+}
+
+
+
+
+
+
+
